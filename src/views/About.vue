36dd4aadcda747a64e633983ed7bd0d3fa65aad5
Oriol Burgaya <oriolbv@gmail.com>
2020-09-13 18:39:52 +0200

    Some useless text removed and table restructuration

--- src/views/About.vue
+++ src/views/About.vue
@@ -15,28 +15,14 @@
           <th>Solution</th> 
         </tr>
         <tr>
-          <td>Design</td>
-          <td>PhotoShop, Figma, Canva, Adobe XD</td> 
-        </tr>
-        <tr>
           <td>Front-end</td>
-          <td>React.js, Vue.js, Nuxt.js, HTML, CSS, JavaScript</td> 
+          <td>React.js, Vue.js, HTML, CSS, JavaScript</td> 
         </tr>
         <tr>
           <td>Back-end</td>
-          <td>Node.js, Express.js, Mongo, GraphQL</td> 
-        </tr>
-        <tr>
-          <td>ML</td>
-          <td>ML5, TensorFlow, Algorithm Design</td> 
-        </tr>
-        <tr>
-          <td>Shop</td>
-          <td>Stripe, PayFast, E-commerce, CMS</td> 
+          <td>PHP, Python, Node.js, Express.js, MongoDB</td> 
         </tr>
       </table>
-       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius consequatur, libero, natus, autem hic saepe exercitationem expedita alias corporis quisquam reiciendis ab quaerat rerum itaque animi repudiandae dolor in fugit ratione inventore molestiae harum accusantium dignissimos. Id praesentium quisquam asperiores ullam quaerat totam, voluptates esse vero repellendus amet, officiis inventore.</p>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius consequatur, libero, natus, autem hic saepe exercitationem expedita alias corporis quisquam reiciendis ab quaerat rerum itaque animi repudiandae dolor in fugit ratione inventore molestiae harum accusantium dignissimos. Id praesentium quisquam asperiores ullam quaerat totam, voluptates esse vero repellendus amet, officiis inventore.</p>
     </div>
   </div>
 </template>
